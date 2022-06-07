47cb04576bed34c2017612766810a7422d3cf527
mardiev.dev <mardiev2125@gmail.com>
2022-06-07 12:52:08 -0500

    experianse section has been completed at all

--- src/I've Worked/IveWorked.js
+++ src/I've Worked/IveWorked.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 class IveWorked extends Component {
     render() {
+        const { expHandler, newList } = this.props
+        const pos = newList?.[0]
+
         return (
             <div className='worked'>
                 <div className="card">
@@ -11,22 +14,23 @@ class IveWorked extends Component {
 
                     <div className="exp__row">
                         <ul className='exp__row-lists'>
-                            <li>Upstatement</li>
-                            <li>Scout</li>
-                            <li>Apple</li>
-                            <li>Scout</li>
-                            <li>Starry</li>
-                            <li>MullenLowe</li>
+                            <li onClick={() => expHandler(1)}>Upstatement</li>
+                            <li onClick={() => expHandler(2)}>Scout</li>
+                            <li onClick={() => expHandler(3)}>Apple</li>
+                            <li onClick={() => expHandler(4)}>Scout</li>
+                            <li onClick={() => expHandler(5)}>Starry</li>
+                            <li onClick={() => expHandler(6)}>MullenLowe</li>
                         </ul>
+
                         <div className="exp">
                             <div className="exp__title">
-                                <h3>Software Engineer Co-op <span>@ Starry</span></h3>
-                                <p className='exp__date'>July - December 2016</p>
+                                <h3>{pos?.title} <span>{pos?.titleSpan}</span></h3>
+                                <p className='exp__date'>{pos?.date}</p>
                             </div>
                             <ul className='exp__overview'>
-                                <li>Write modern, performant, maintainable code for a diverse array of client and internal projects</li>
-                                <li>Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify</li>
-                                <li>Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis</li>
+                                <li>{pos?.firsP}</li>
+                                <li>{pos?.secondP}</li>
+                                <li>{pos?.thirdP}</li>
                             </ul>
                         </div>
                     </div>
--- src/Main.jsx
+++ src/Main.jsx
@@ -14,7 +14,64 @@ export default class Main extends Component {
         this.state = {
             logo: false,
             scrollShow: true,
-            scrollPos: 0
+            scrollPos: 0,
+            experiance: [
+                {
+                    id: 1,
+                    title: "Engineer",
+                    titleSpan: "@ Upstatement",
+                    date: "May 2018 - Present",
+                    firsP: "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+                    secondP: "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Gatsby, React, Craft, WordPress, Prismic, and Netlify",
+                    thirdP: "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis"
+                },
+                {
+                    id: 2,
+                    title: "Studio Developer",
+                    titleSpan: "@ Scout",
+                    date: "January - April 2018",
+                    firsP: "Worked with a team of three designers to build a marketing website and e-commerce platform for blistabloc, an ambitious startup originating from Northeastern",
+                    secondP: "Helped solidify a brand direction for blistabloc that spans both packaging and web",
+                    thirdP: "Interfaced with clients on a weekly basis, providing technological expertise"
+                },
+                {
+                    id: 3,
+                    title: "UI Engineer Co-op",
+                    titleSpan: "@ Apple",
+                    date: "July - December 2017",
+                    firsP: "Developed and shipped highly interactive web applications for Apple Music using Ember.js",
+                    secondP: "Built and shipped the Apple Music Extension within Facebook Messenger leveraging third-party and internal APIs",
+                    thirdP: "Architected and implemented the front-end of Apple Music's embeddable web player widget, which lets users log in and listen to full songs in the browser"
+                },
+                {
+                    id: 4,
+                    title: "Studio Developer",
+                    titleSpan: "@ Scout",
+                    date: "January - June 2017",
+                    firsP: "Collaborated with a small team of student designers to spearhead a new brand and design system for Scout’s inaugural student-led design conference at Northeastern",
+                    secondP: "Worked closely with designers and management team to develop, document, and manage the conference’s marketing website using Jekyll, Sass, and JavaScript",
+                    thirdP: "Contributed extensively to MusicKit.js, a JavaScript framework that allows developers to add an Apple Music player to their web apps"
+                },
+                {
+                    id: 5,
+                    title: "Software Engineer Co-op",
+                    titleSpan: "@ Starry",
+                    date: "July - December 2016",
+                    firsP: "Engineered and maintained major features of Starry's customer-facing web app using ES6, Handlebars, Backbone, Marionette and CSS",
+                    secondP: "Proposed and implemented scalable solutions to issues identified with cloud services and applications responsible for communicating with Starry Station",
+                    thirdP: "nterfaced with user experience designers and other developers to ensure thoughtful and coherent user experiences across Starry’s iOS and Android mobile apps"
+                },
+                {
+                    id: 6,
+                    title: "Creative Technologist Co-op",
+                    titleSpan: "@ MullenLowe",
+                    date: "July - December 2015",
+                    firsP: "Developed and maintained code for in-house and client websites primarily using HTML, CSS, Sass, JavaScript, and jQuery",
+                    secondP: "Manually tested sites in various browsers and mobile devices to ensure cross-browser compatibility and responsiveness",
+                    thirdP: "Clients included JetBlue, Lovesac, U.S. Cellular, U.S. Department of Defense, and more"
+                }
+            ],
+            newExpList: {}
 
         }
     }
@@ -23,26 +80,21 @@ export default class Main extends Component {
         // setTimeout(() => {
         //     this.setState({ logo: !this.state.logo })
         // }, 5000)
-        window.addEventListener("scroll", this.scrollHandler)
+        // window.addEventListener("scroll", this.scrollHandler)
+        this.experianceHandler()
     }
 
-    componentWillUnmount() {
-        window.removeEventListener("scroll", this.scrollHandler)
-    }
-
-    scrollHandler = () => {
-        console.log(document.body.getBoundingClientRect())
-
+    experianceHandler = (id = 1) => {
+        const { experiance } = this.state
+        const filtered = experiance.filter(item => item.id == id)
         this.setState({
-            scrollPos: document.body.getBoundingClientRect().top,
-            scrollShow: document.body.getBoundingClientRect().top > this.state.scrollPos
+            newExpList: filtered
         })
-
     }
 
     render() {
-        console.log(this.state.scrollPos, this.state.scrollShow)
-        const { logo, scrollPos, scrollShow } = this.state
+        const { logo, experiance, newExpList } = this.state
+        console.log(experiance)
         return (<div>
             {logo ? <Logo /> : ""}
             <Navbar />
@@ -50,7 +102,7 @@ export default class Main extends Component {
                 <Header />
                 <div className="row">
                     <AboutMe />
-                    <IveWorked />
+                    <IveWorked expHandler={this.experianceHandler} newList={newExpList} />
                 </div>
             </div>
         </div>);
